9e3fea445a438cac33a295b0982e6714a97f831e
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2020-09-29 21:26:29 +0800

    d

--- src/modules/product/Product.vue
+++ src/modules/product/Product.vue
@@ -1,24 +1,35 @@
 <template>
   <div id="container">
+    <h1>WAWEN'S UBE HALAYA : Products</h1>
     <div class="row">
       <div class="column">
-        <img :src="require('@/assets/halaya.jpg')" alt="Snow" style="width:100%">
+        <img :src="require('@/assets/halaya.jpg')" alt="Snow" style="width:100%;height:300px;">
+        <label>this is he name</label>
+        <img id='cart' :src="require('@/assets/logo.png')">
       </div>
       <div class="column">
-        <img :src="require('@/assets/cake.png')" alt="Forest" style="width:100%">
+        <img :src="require('@/assets/cake.png')" alt="Forest" style="width:100%;height:300px;">
       </div>
       <div class="column">
-        <img :src="require('@/assets/buchi.jpg')" alt="Mountains" style="width:100%">
+        <img :src="require('@/assets/buchi.jpg')" alt="Mountains" style="width:100%;height:300px;">
       </div>
     </div>
   </div>
 </template>
 <style scoped lang="scss">
+h1 {
+  color: #8028d8;
+  font-family: Segoe Script;
+  font-weight: bolder;
+  text-align: center;
+  padding-bottom: 2%; 
+}
+
 #container {
   width: 80%;
   margin-left: 10%;
   margin-right: 10%;
-  margin-top: 10%;
+  padding-top: 5%;
 }
 
 * {
@@ -26,9 +37,18 @@
 }
 
 .column {
-  float: left;
-  width: 33.33%;
+  float: center;
+  width: 30%;
   padding: 5px;
+  margin-left: 10px;
+  height: 400px;
+  border: 1px solid #000;
+}
+
+#cart{
+  width: 50px;
+  height: 50px;
+  float: right;
 }
 
 /* Clearfix (clear floats) */
